f5659e4a0236501ec7bdbefa0a51b20f4e7e9994
Amr Hany <inertgrey@outlook.com>
2019-08-03 15:55:44 +0200

    Added project Mirai assets

---
+++ Assets/Mirai Assets/Assets/StartEngine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartEngine : MonoBehaviour
+{
+    bool shouldStart = false;
+    Transform propeller1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        propeller1 = transform.Find("propeller 1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (shouldStart)
+        {
+           transform.Rotate(Vector3.up, 360 * Time.deltaTime);
+        }
+
+    }
+    
+    public void SetStart()
+    {
+        shouldStart = true;
+    }
+
+}
